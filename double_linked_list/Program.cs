9da3ae4b885de8613591bb03bef1d2c2d45bf7a9
LISA <ASUS@LAPTOP-67G15PD7>
2022-11-14 17:25:29 +0700

    add the begining of data

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -90,6 +90,19 @@ namespace double_linked_list
                 current = current.next) { }
             return (current != null);
         }
+        public bool dellNode(int rollNo)
+        {
+            Node previous, current;
+            previous = current = null;
+            if (search(rollNo, ref previous, ref current) == false)
+                return false;
+            //the begining of data
+            if (current.next == null)
+            {
+                previous.next = null;
+                return true;
+            }
+        }
     }
     internal class Program
     {
