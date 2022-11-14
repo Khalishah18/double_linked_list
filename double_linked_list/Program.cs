db97313c39e68841c45d2dfdce244988770d0fb2
LISA <ASUS@LAPTOP-67G15PD7>
2022-11-14 17:19:18 +0700

    add public bool search

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -79,6 +79,16 @@ namespace double_linked_list
                 previous.next = newNode;
                 return;
             }
+            current.prev = newNode;
+            previous.next = newNode;
+        }
+
+        public bool search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current =START; current != null &&
+                rollNo != current.noMhs; previous = current,
+                current = current.next) { }
+            return (current != null);
         }
     }
     internal class Program
