901a8fa3f34ba19c05f099758418beb24969ef13
LISA <ASUS@LAPTOP-67G15PD7>
2022-11-14 18:43:15 +0700

    add case 1-3

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -173,7 +173,36 @@ namespace double_linked_list
                     Console.WriteLine("5. Search for a record in the list");
                     Console.WriteLine("6. Exit\n");
                     Console.WriteLine("Enter your choice (1-6): ");
-
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.addNode();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Console.Write("\nEnter the roll number of the student" + "whose record is to be deleted: ");
+                                int rollNo = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.dellNode(rollNo) == false)
+                                    Console.WriteLine("Record not found");
+                                else
+                                    Console.WriteLine("Record with roll number " + rollNo + "deleted \n");
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.ascending();
+                            }
+                            break;
+                    }
                 }
             }
         }
